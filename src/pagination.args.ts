3fb639420e14d8636e8c5966ba6d3c570f36a93e
Alireza Zamani <alirezazamani2922@gmail.com>
2021-11-19 20:04:43 +0330

    fix: Make args nullable

--- src/pagination.args.ts
+++ src/pagination.args.ts
@@ -3,16 +3,16 @@ import { ArgsType, Field, Int } from '@nestjs/graphql';
 @ArgsType()
 export class PaginationArgs {
   // Forward pagination arguments
-  @Field(() => Int)
+  @Field(() => Int, { nullable: true })
   first?: number;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   after?: string;
 
   // Backward pagination arguments
-  @Field(() => Int)
+  @Field(() => Int, { nullable: true })
   last?: number;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   before?: string;
 }
