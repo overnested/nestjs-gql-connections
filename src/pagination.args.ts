f8ece15059fb8702c6a9bb2f9e6f54bce03a2e3c
Alireza Zamani <alirezazamani2922@gmail.com>
2021-11-19 19:46:53 +0330

    fix: Add field decorator to args object

--- src/pagination.args.ts
+++ src/pagination.args.ts
@@ -1,11 +1,15 @@
-import { ArgsType } from '@nestjs/graphql';
+import { ArgsType, Field, Int } from '@nestjs/graphql';
 
 @ArgsType()
 export class PaginationArgs {
   // Forward pagination arguments
+  @Field(() => Int)
   first?: number;
+  @Field()
   after?: string;
   // Backward pagination arguments
+  @Field(() => Int)
   last?: number;
+  @Field()
   before?: string;
 }
